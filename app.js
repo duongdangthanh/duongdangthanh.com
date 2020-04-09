762289e670b83612c38eae0202cea07427c5833c
Dương Đăng Thanh <duongdangthanh01@gmail.com>
2020-04-09 23:03:12 +0700

    updated app.js

--- app.js
+++ app.js
@@ -1,8 +1,21 @@
 const express = require('express');
 const app = express();
 const path = require('path');
+const bodyParser = require('body-parser');
+
+app.use(bodyParser.urlencoded({ extended: false }))
+app.use(bodyParser.json())
 
 app.use(express.static(__dirname + '/public'));
+
+app.post("/login", (req, res) => {
+    const { userName, password } = req.body;
+    return res.send({ userName, password });
+})
+
+app.get("/user", (req, res) => {
+    return res.send("Success");
+})
 
 var server = app.listen(80, function () {
    var host = server.address().address
